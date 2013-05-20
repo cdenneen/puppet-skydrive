a4cf883bfd985bde0b0c004926e2f6abe45005c5
Juan Ignacio Donoso <jidonoso@gmail.com>
2013-05-20 17:41:20 -0400

    Change the package provider to pkgdmg

--- spec/classes/skydrive_spec.rb
+++ spec/classes/skydrive_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe 'skydrive' do
   it do
     should contain_package('SkyDrive').with({
-      :provider => 'apple',
-      :source   => '/var/tmp/SkyDrive.pkg',
+      :provider => 'pkgdmg',
+      :source   => 'http://wl.dlservice.microsoft.com/download/1/A/5/1A5C4419-BF0B-42BB-BCAA-F79CFD26044A/SkyDrive.pkg',
     })
   end
 end
